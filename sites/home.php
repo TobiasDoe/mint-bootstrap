b429e9198a26f9e167234b911dabb59a9a185d7d
TobiasDoe <developer@onmyown.at>
2019-07-25 11:51:25 +0200

    update

--- sites/home.php
+++ sites/home.php
@@ -1,5 +1,5 @@
 <div id="page" class="container-fluid p-0">
-	<div class="main-banner" style="background-image: url(images/mint.jpg)">
+	<div class="main-banner">
 		<div class="banner-content d-flex justify-content-center align-items-center flex-column display-1">
 			<h1 class="main-text m-5 p-5">Mint</h1>
 		</div>
