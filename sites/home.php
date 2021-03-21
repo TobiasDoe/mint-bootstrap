e4dffcbbc17eb2acee59c7dbbd83338767d9e04a
Tobias Doe <developer@onmyown.at>
2021-03-21 09:00:31 +0100

    modif: name

--- sites/home.php
+++ sites/home.php
@@ -1,7 +1,7 @@
 <div id="page" class="container-fluid p-0 m-0">
 	<div class="main-banner">
 		<div class="banner-content d-flex justify-content-center align-items-center flex-column display-1">
-			<h1 class="main-text m-5 p-5"><i class="fad fa-dog-leashed mr-3"></i>Mint</h1>
+			<h1 class="main-text m-5 p-5"><i class="fad fa-dog-leashed mr-3"></i>Mint-Bootstrap</h1>
 		</div>
 	</div>
 </div>
